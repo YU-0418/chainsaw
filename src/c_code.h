ac0d3e78e60cc3b2f9b31c46975f874ca35a2dd3
yu <YU__2001@outlook.com>
2025-09-17 16:48:28 +0800

    add define

---
+++ src/c_code.h
@@ -0,0 +1,10 @@
+#ifndef CHAINSAW_C_CODE
+#define CHAINSAW_C_CODE
+
+#ifdef ONLY_C
+// 暂时先不加入编译动态/静态库
+#include "c_code/c_string.h";
+#include "c_code/c_define.h"
+#endif // ONLY_C
+
+#endif // !CHAINSAW_C_CODE
